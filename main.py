384501ed68d5a648542447fa437247d555717b8f
Macasorry <150623629+Macasorry@users.noreply.github.com>
2024-04-07 16:35:02 +0300

    main & pe file adding

---
+++ main.py
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+import numpy as np
+import cv2 as cv
+from picture_editing import VideoEditor
+
+
+cap = cv.VideoCapture("Traffic IP Camera video (1).mp4")
+
+bg_subs = cv.createBackgroundSubtractorMOG2()
+history = 90
+learning_rate = 1.0/history
+while cap.isOpened():
+
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frame = VideoEditor.frame_edit(frame, 0.5)
+    mask = bg_subs.apply(frame, learningRate=learning_rate)
+    mask = VideoEditor.mask_edit(mask, 3, 1)
+    out = frame.copy()
+    # result = cv.bitwise_and(out, out, mask=mask)
+    # cv.imshow('res', mask)
+    VideoEditor().find_ctr(mask, out)
+    cv.imshow("input", out)
+    c = cv.waitKey(10)
+    if c == 27:
+        break
+cap.release()
+cv.destroyAllWindows()
--- util/picture_editing.py
+++ util/picture_editing.py
@@ -3,3 +3,25 @@
 # файл с методами для работы с изображением
 #
 #####
+import cv2 as cv
+import numpy as np
+
+class VideoEditor:
+    @staticmethod
+    def frame_edit(frame, scale):
+        frame = cv.resize(frame, None, fx=scale, interpolation=cv.INTER_AREA)
+        return frame
+
+    @staticmethod
+    def mask_edit(frame, kernel, iterations):
+        kernel = np.ones((kernel, kernel), np.uint8)
+        opening = cv.morphologyEx(frame, cv.MORPH_OPEN, kernel)
+        # dilation = cv.dilate(frame, kernel, iterations=iterations)
+        return opening
+    @staticmethod
+    def find_ctr(frame):
+        ret, thresh = cv.threshold(frame, 170, 255, 0)
+        im2, contours, hierarchy = cv.findContours(
+            thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE
+        )
+        cv.drawContours(res, contours, )
